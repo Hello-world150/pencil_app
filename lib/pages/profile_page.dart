78964970fc8026eaa6076e4fa18352fc5f139501
Haolin Qin <qhl314159265@126.com>
2025-08-04 18:41:30 +0800

    feat(profile_page): add divider and collections show case

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'settings_page.dart';
+import '/components/divider.dart';
+import '/components/collection_gridview.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -22,23 +24,36 @@ class ProfilePage extends StatelessWidget {
       ),
       body: Padding(
         // set the padding around the page
-        padding: const EdgeInsets.all(35.0),
+        padding: const EdgeInsets.all(20.0),
 
         // this is the user information
-        child: Row(
+        child: Column(
           children: [
-            // this is the user avatar
-            CircleAvatar(
-              radius: 24,
-              backgroundImage: AssetImage(
-                'assets/profile_picture.png',
-              ), // Replace with your image
+            Row(
+              children: [
+                // this is the user avatar
+                CircleAvatar(
+                  radius: 24,
+                  backgroundImage: AssetImage(
+                    'assets/profile_picture.png',
+                  ), // Replace with your image
+                ),
+
+                const SizedBox(width: 20),
+
+                // this is the user name
+                Text(
+                  "Username",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ],
             ),
 
-            const SizedBox(width: 20),
+            // a divider to divide the user information and the content
+            const MyDivider(),
 
-            // this is the user name
-            Text("Username", style: Theme.of(context).textTheme.titleMedium),
+            // list the user collections
+            const CollectionGridview(),
           ],
         ),
       ),
